da3c422a898ae6cf0c8ae6bcd1e2507bb47092ad
leandrocarlos.souza@gmail.com <leandrocarlos.souza@gmail.com@58edc5d6-6154-0410-bc03-934b031377ca>
2008-08-22 15:15:33 +0000

    versao inicial da interface grafica... Alteracao do eclipse, que agora contem a perspectiva presley e uma nova view.
    ssEsta linha, e as abaixo, serão ignoradas--
    
    A    presley/code/src/ProjectPresley
    A    presley/code/src/ProjectPresley/.classpath
    A    presley/code/src/ProjectPresley/META-INF
    A    presley/code/src/ProjectPresley/META-INF/MANIFEST.MF
    A    presley/code/src/ProjectPresley/.project
    A    presley/code/src/ProjectPresley/src
    A    presley/code/src/ProjectPresley/src/projectpresley
    A    presley/code/src/ProjectPresley/src/projectpresley/view
    A    presley/code/src/ProjectPresley/src/projectpresley/view/Mensagens.java
    A    presley/code/src/ProjectPresley/src/projectpresley/Activator.java
    A    presley/code/src/ProjectPresley/src/projectpresley/perspectives
    A    presley/code/src/ProjectPresley/src/projectpresley/perspectives/RelEngPerspective.java
    A    presley/code/src/ProjectPresley/bin
    A    presley/code/src/ProjectPresley/bin/projectpresley
    AM   presley/code/src/ProjectPresley/bin/projectpresley/Activator.class
    A    presley/code/src/ProjectPresley/bin/projectpresley/view
    AM   presley/code/src/ProjectPresley/bin/projectpresley/view/Mensagens.class
    A    presley/code/src/ProjectPresley/bin/projectpresley/perspectives
    AM   presley/code/src/ProjectPresley/bin/projectpresley/perspectives/RelEngPerspective.class
    A    presley/code/src/ProjectPresley/build.properties
    A    presley/code/src/ProjectPresley/icons
    AM   presley/code/src/ProjectPresley/icons/releng_gears.gif
    A    presley/code/src/ProjectPresley/plugin.xml
    A    presley/code/src/ProjectPresley/.settings
    A    presley/code/src/ProjectPresley/.settings/org.eclipse.jdt.core.prefs

---
+++ presley/code/src/ProjectPresley/src/projectpresley/view/Mensagens.java
@@ -0,0 +1,38 @@
+package projectpresley.view;
+
+/* Desenvolvido por Leandro Carlos e Samara Martins */
+
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.part.ViewPart;
+
+public class Mensagens extends ViewPart {
+
+	public Mensagens() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+		final Tree tree = new Tree(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CHECK);
+		TreeItem item = new TreeItem(tree, SWT.NONE);
+		item.setText("Problema");	
+		
+		final TreeItem radio1 = new TreeItem(item, SWT.NONE);
+		radio1.setText("Bope");
+
+		final TreeItem radio2 = new TreeItem(item,  SWT.NONE);
+	    radio2.setText("GigaBug");	
+
+	}
+
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
