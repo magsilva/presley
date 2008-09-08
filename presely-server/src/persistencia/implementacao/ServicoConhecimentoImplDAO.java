d5ceb96c6acfe84481f79ba728cb0c1ef8ed32ca
joaopaulopbjp <joaopaulopbjp@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-08 17:58:33 +0000

    Inclusão de Classes que implementam os serviços da persistencia.

---
+++ presely-server/src/persistencia/implementacao/ServicoConhecimentoImplDAO.java
@@ -0,0 +1,203 @@
+package persistencia.implementacao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import beans.Conhecimento;
+
+import persistencia.MySQLConnectionFactory;
+import persistencia.interfaces.ServicoConhecimento;
+
+/**
+ * 
+ * @author Amilcar Jr
+ * Essa classe contem a implementacao das operacoes para administrar um conhecimento.
+ * 
+ * Última modificacao: 03/09/2008 por Amilcar Jr
+ */
+
+public class ServicoConhecimentoImplDAO implements ServicoConhecimento{
+
+	public boolean atualizarConhecimento(String nome, String novoNome,
+			String descricao) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " UPDATE conhecimento SET nome = '"+novoNome+"',"+
+						 " descricao = '"+descricao+"' "+
+						 " WHERE nome = '"+nome+"';";
+			
+			System.out.println(SQL);
+			stm.execute(SQL);
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+				
+		return true;
+	}
+
+	public boolean conhecimentoExiste(String nome) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " SELECT * FROM conhecimento WHERE "+
+						 " nome = '"+nome+"';";
+				
+			
+			System.out.println(SQL);
+			ResultSet rs = stm.executeQuery(SQL);
+			
+			if (rs.next()){
+				return true;
+			}else{
+				return false;
+			}
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+	}
+
+	public boolean criarConhecimento(String nome, String descricao) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " INSERT INTO conhecimento " +
+					     " VALUES('"+nome+"','"+
+						  descricao+"');";
+			
+			System.out.println(SQL);
+			stm.execute(SQL);
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+				
+		return true;
+	}
+
+	public boolean removerConhecimento(String nome) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			if (this.conhecimentoExiste(nome)){
+				String SQL = " DELETE FROM conhecimento WHERE " +
+							 " nome = '"+nome+"';";
+				
+				System.out.println(SQL);
+				stm.execute(SQL);
+				return true;
+			}else{
+				return false;
+			}
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+				
+	}
+
+	public Conhecimento getConhecimento(String nome) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " SELECT * FROM conhecimento WHERE "+
+						 " nome = '"+nome+"';";
+				
+			
+			System.out.println(SQL);
+			ResultSet rs = stm.executeQuery(SQL);
+			
+			if (rs.next()){
+				
+				Conhecimento conhecimento = new Conhecimento();
+				
+				conhecimento.setNome(rs.getString(1));
+				conhecimento.setDescricao(rs.getString(2));
+				
+				return conhecimento;
+				
+			}else{
+				return null;
+			}
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		} finally {
+	         try {
+	             conn.close();	            
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+		
+	}
+
+}
+
---
+++ presely-server/src/persistencia/implementacao/ServicoProblemaImplDAO.java
@@ -0,0 +1,251 @@
+package persistencia.implementacao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import beans.Problema;
+import persistencia.MySQLConnectionFactory;
+import persistencia.interfaces.ServicoAtividade;
+import persistencia.interfaces.ServicoProblema;
+
+public class ServicoProblemaImplDAO implements ServicoProblema{
+
+	public boolean atualizarStatusDoProblema(int id, boolean status) {
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL;
+			
+			if(status){
+			 SQL = " UPDATE problema SET resolvido = 1"+
+				   " WHERE id = "+id+";";
+			}else{
+			 SQL = " UPDATE problema SET resolvido = 0"+
+				   " WHERE id = "+id+";";
+			}
+			System.out.println(SQL);
+			stm.execute(SQL);
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+				
+		return true;
+
+	}
+
+	public boolean cadastrarProblema(int idAtividade, String descricao,
+			Date dataDoRelato, String mensagem) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " INSERT INTO problema(atividade_id,descricao,dataRelato,mensagem) " +
+					     " VALUES("+idAtividade+",'"+
+						   descricao+"','"+dataDoRelato+"','"+mensagem+"');";
+			
+			System.out.println(SQL);
+			stm.execute(SQL);
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+				
+		return true;
+		
+	}
+
+	public ArrayList<Problema> listarProblemasDaAtividade(int idAtividade) {
+		
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		ArrayList<Problema> list = new ArrayList<Problema>();
+		ServicoAtividade sa = new ServicoAtividadeImplDAO();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " SELECT * FROM problema WHERE "+
+						 " atividade_id = "+idAtividade+" ORDER BY atividade_id;";
+				
+			
+			System.out.println(SQL);
+			ResultSet rs = stm.executeQuery(SQL);
+			
+			while (rs.next()){
+				
+				Problema p = new Problema();
+				
+				p.setId(rs.getInt(1));
+				p.setAtividade(sa.getAtividade(rs.getInt(2)));
+				p.setDescricao(rs.getString(3));
+				p.setResolvido(rs.getBoolean(4));
+				p.setData(rs.getDate(5));
+				p.setMensagem(rs.getString(6));
+				
+				list.add(p);
+				
+			}
+			return list;
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+		
+	}
+
+	public boolean removerProblema(int id) {
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			if (this.problemaExiste(id)){
+				String SQL = " DELETE FROM problema WHERE " +
+							 " id = "+id+";";
+				
+				System.out.println(SQL);
+				stm.execute(SQL);
+				return true;
+			
+			} else {
+				return false;
+			}
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+	}
+
+	public boolean problemaExiste(int id) {
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " SELECT * FROM problema WHERE "+
+						 " id = "+id+" ORDER BY id;";
+				
+			
+			System.out.println(SQL);
+			ResultSet rs = stm.executeQuery(SQL);
+			
+			if (rs.next()){
+				return true;
+			}else{
+				return false;
+			}
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return false;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+	}
+
+	public Problema getProblema(int id) {
+		MySQLConnectionFactory factory = new MySQLConnectionFactory();
+		ServicoAtividade sa = new ServicoAtividadeImplDAO();
+		Connection conn = factory.getConnection();
+		
+		try {
+		
+			Statement stm = conn.createStatement();
+		
+			String SQL = " SELECT * FROM problema WHERE "+
+						 " id = "+id+";";
+				
+			
+			System.out.println(SQL);
+			ResultSet rs = stm.executeQuery(SQL);
+			
+			if (rs.next()){
+				
+				Problema p = new Problema();
+				
+				p.setId(rs.getInt(1));
+				p.setAtividade(sa.getAtividade(rs.getInt(2)));
+				p.setDescricao(rs.getString(3));
+				p.setResolvido(rs.getBoolean(4));
+				p.setData(rs.getDate(5));
+				p.setMensagem(rs.getString(6));
+				
+				return p;
+			}else{
+				return null;
+			}
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		} finally {
+	         try {
+	             conn.close();
+	           } catch (SQLException onConClose) {
+	             System.out.println(" Houve erro no fechamento da conexão ");
+	             onConClose.printStackTrace();	             
+	           }
+	         }
+	}
+
+}
+
