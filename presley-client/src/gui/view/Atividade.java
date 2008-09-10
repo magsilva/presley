bf4785232b2584260e468a00bb9bae24a46d5a89
joaopaulopbjp <joaopaulopbjp@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-10 20:36:20 +0000

    Correção de bug que não permitia atualizar as arvores de conhecimento e problema quando ocorria um evento de clique do mouse.

--- presley-client/src/gui/view/Atividade.java
+++ presley-client/src/gui/view/Atividade.java
@@ -4,34 +4,25 @@ import gui.view.comunication.ViewComunication;
 
 import java.util.ArrayList;
 
-import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Region;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Layout;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.part.ViewPart;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.refresh.IRefreshMonitor;
-import org.eclipse.core.resources.refresh.IRefreshResult;
-import org.eclipse.core.resources.refresh.RefreshProvider;
 
 
 public class Atividade extends ViewPart {
 
 	private ArrayList<Button> buttons;
+	private Label conhecimentoLabel;
+	private Tree conhecimentoTree;
+	private Label problemaLabel;
+	private Tree problemaTree;
+	
 	private ViewComunication viewComunication;
 
 	public Atividade()
@@ -65,6 +56,11 @@ public class Atividade extends ViewPart {
 			b.setSize(200, 25);
 			this.buttons.add(b);	
 		}
+		
+		conhecimentoLabel = new Label(parent, SWT.BORDER | SWT.CENTER);
+		conhecimentoTree = new Tree(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CHECK | SWT.SCROLL_LINE);
+		problemaLabel = new Label(parent, SWT.BORDER | SWT.CENTER);
+		problemaTree = new Tree(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CHECK | SWT.SCROLL_LINE);
 	
 		for(final Button b : buttons)
 		{
@@ -75,22 +71,19 @@ public class Atividade extends ViewPart {
 				}
 	
 				public void mouseDown(MouseEvent e) {
-					String selection = b.getText();
-					Composite parent = b.getParent();
+					String selection = ((Button)e.getSource()).getText();
 					int depoisButoes = viewComunication.getAtividades().size()*25;		
-			
-					parent.setRedraw(false);
-											
-					Label conhecimentoLabel = new Label(parent, SWT.BORDER | SWT.CENTER);
+										
 					conhecimentoLabel.setText("Conhecimentos Envolvidos");
 					conhecimentoLabel.setLocation(0, depoisButoes + 5);
 					conhecimentoLabel.setSize(200, 20);	
 									
-					final Tree conhecimentoTree = new Tree(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CHECK | SWT.SCROLL_LINE);
 					conhecimentoTree.setLocation(0, depoisButoes + 30);
 					conhecimentoTree.setSize(200, 150);
 					
-						ArrayList<String> conhecimentos = viewComunication.getConhecimentosEnvolvidos(selection);
+					ArrayList<String> conhecimentos = viewComunication.getConhecimentosEnvolvidos(selection);
+					
+					conhecimentoTree.removeAll();
 						if(conhecimentos != null)
 							for(String c : conhecimentos)
 							{
@@ -98,27 +91,23 @@ public class Atividade extends ViewPart {
 								item.setText(c);
 							}				
 
-					Label problemaLabel = new Label(parent, SWT.BORDER | SWT.CENTER);
 					problemaLabel.setText("Problemas Encontrados");
 					problemaLabel.setLocation(0, depoisButoes + 185);
 					problemaLabel.setSize(200, 20);
 					
-					final Tree problemaTree = new Tree(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CHECK | SWT.SCROLL_LINE);
 					problemaTree.setLocation(0, depoisButoes + 210);
 					problemaTree.setSize(200, 150);
 					
 						ArrayList<String> problemas = viewComunication.getProblemas(selection);
-						
+					
+					problemaTree.removeAll();						
 						if(problemas != null)
 							for(String c : problemas)
 							{
 								TreeItem item = new TreeItem(problemaTree, SWT.NONE);
 								item.setText(c);
 							}
-						
-						parent.update();
-						parent.setRedraw(true);
-						parent.redraw();		
+								
 				}
 				
 				public void mouseUp(MouseEvent e) {
