e497b94b965befe29500f10b1f60fadf9f2563c8
joaopaulopbjp <joaopaulopbjp@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-13 20:18:01 +0000

    Adição de Ação para executar a Wizard para remove atividade.

---
+++ presley-client/src/gui/action/RunRemoveAtividadeWizardAction.java
@@ -0,0 +1,69 @@
+package gui.action;
+
+import gui.view.Atividade;
+import gui.wizard.RemoveAtividadeWizard;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+
+public class RunRemoveAtividadeWizardAction extends Action implements
+		IWorkbenchWindowActionDelegate {
+	private Atividade atividade;
+
+	public RunRemoveAtividadeWizardAction() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public RunRemoveAtividadeWizardAction(Atividade a) {
+		// TODO Auto-generated constructor stub
+		this.atividade = a;
+	}
+
+	public RunRemoveAtividadeWizardAction(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RunRemoveAtividadeWizardAction(String text, ImageDescriptor image) {
+		super(text, image);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RunRemoveAtividadeWizardAction(String text, int style) {
+		super(text, style);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init(IWorkbenchWindow arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void run(IAction arg0) {
+		RemoveAtividadeWizard wizard = new RemoveAtividadeWizard(this.atividade);
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		WizardDialog dialog = new WizardDialog(shell,wizard);
+		dialog.create();
+		dialog.open();
+	
+
+	}
+
+	public void selectionChanged(IAction arg0, ISelection arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
