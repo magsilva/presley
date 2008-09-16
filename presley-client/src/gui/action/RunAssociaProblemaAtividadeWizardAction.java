a2aed9a5b84c8c868616f7863f1eda6af5551422
joaopaulopbjp <joaopaulopbjp@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-16 20:02:03 +0000

    Inclusão de wizard para associar um problema a atividade e sua ação

---
+++ presley-client/src/gui/action/RunAssociaProblemaAtividadeWizardAction.java
@@ -0,0 +1,77 @@
+package gui.action;
+
+import gui.view.Atividade;
+import gui.wizard.AdicionaAtividadeWizard;
+import gui.wizard.AssociaProblemaAtividadeWizard;
+import gui.wizard.LoginWizard;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+
+import beans.TipoAtividade;
+
+public class RunAssociaProblemaAtividadeWizardAction extends Action implements
+		IWorkbenchWindowActionDelegate {
+	private Atividade atividade;
+	private TipoAtividade tipoAtividadeAssociada;
+
+	public RunAssociaProblemaAtividadeWizardAction() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public RunAssociaProblemaAtividadeWizardAction(Atividade a, TipoAtividade tipoAtividadeAssociada) {
+		// TODO Auto-generated constructor stub
+		this.atividade = a;
+		this.tipoAtividadeAssociada = tipoAtividadeAssociada;
+	}
+
+	public RunAssociaProblemaAtividadeWizardAction(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RunAssociaProblemaAtividadeWizardAction(String text, ImageDescriptor image) {
+		super(text, image);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RunAssociaProblemaAtividadeWizardAction(String text, int style) {
+		super(text, style);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init(IWorkbenchWindow arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void run(IAction arg0) {
+		AssociaProblemaAtividadeWizard wizard = new AssociaProblemaAtividadeWizard(this.atividade);
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		WizardDialog dialog = new WizardDialog(shell,wizard);
+		dialog.create();
+		dialog.open();
+		dialog.close();
+		wizard.dispose();
+	
+
+	}
+
+	public void selectionChanged(IAction arg0, ISelection arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
