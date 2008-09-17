6f0a27e14d7738bf7ab9957e2c230ea3bc7217b5
thales.pordeus <thales.pordeus@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-17 20:40:06 +0000

    Implementado o construtor.

--- presely-server/src/validacao/excessao/DesenvolvedorInexistenteException.java
+++ presely-server/src/validacao/excessao/DesenvolvedorInexistenteException.java
@@ -1,5 +1,9 @@
 package validacao.excessao;
 
 public class DesenvolvedorInexistenteException extends Exception{
-
+	
+	public DesenvolvedorInexistenteException(){
+		super("Não há desenvolvedores aptos a esse conhecimento!");
+	}
+	
 }
