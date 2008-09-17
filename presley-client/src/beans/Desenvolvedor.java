12803c8050ffd7d08aff0fef596206e42c23cc4e
joaopaulopbjp <joaopaulopbjp@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-17 20:48:38 +0000

    Alteração de Desenvolvedor

---
+++ presley-client/src/beans/Desenvolvedor.java
@@ -0,0 +1,69 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Amilcar Jr
+ * Essa classe contém dados inerentes a um desenvolvedor.
+ * 
+ * Última modificacao: 03/08/2008 por Amilcar Jr
+ * 
+ */
+
+public class Desenvolvedor implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String nome;
+	private String localidade;
+	private ArrayList<Conhecimento> listaConhecimento;
+	private String senha;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocalidade() {
+		return localidade;
+	}
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+	public ArrayList<Conhecimento> getListaConhecimento() {
+		return listaConhecimento;
+	}
+	public void setListaConhecimento(ArrayList<Conhecimento> listaConhecimento) {
+		this.listaConhecimento = listaConhecimento;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	
+	@Override
+	public boolean equals(Object arg){
+		return this.nome.equals(((Desenvolvedor)arg).nome);
+	}
+	
+	@Override
+	public int hashCode(){
+		return this.email.hashCode();
+	}
+	
+}
+
