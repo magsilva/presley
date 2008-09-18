2d608a1411b93e193438299545cefff98640b429
walberjas@gmail.com <walberjas@gmail.com@58edc5d6-6154-0410-bc03-934b031377ca>
2008-09-18 03:03:17 +0000

    Ateração nos parâmetros do método buscaDesenvolvedores

--- presley-client/src/gui/view/comunication/CorePresleyOperations.java
+++ presley-client/src/gui/view/comunication/CorePresleyOperations.java
@@ -2,7 +2,7 @@ package gui.view.comunication;
 
 import java.util.ArrayList;
 
-import excessao.*;
+import excecao.*;
 import beans.Conhecimento;
 import beans.DadosAutenticacao;
 import beans.Desenvolvedor;
@@ -153,7 +153,7 @@ public interface CorePresleyOperations {
 	 * @param int grauDeConfiaca varia de 1 -> 100
 	 * @return true se a desassociação foi realizada com sucesso.
 	 */
-	public ArrayList<Desenvolvedor> buscaDesenvolvedores(Problema problema, ArrayList<Conhecimento> listaConhecimento, int grauDeConfianca);
+	public ArrayList<Desenvolvedor> buscaDesenvolvedores(ArrayList<String> listaConhecimento, int grauDeConfianca);
 
 	/**
 	 * Este método qualifica o desenvolvedor de acordo com as respostas dele aos problemas
