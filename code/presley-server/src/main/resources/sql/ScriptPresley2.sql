b4a6de3dd8664313a38a7a316d92b75cfd6a436f
Cleytonct <Cleytonct@58edc5d6-6154-0410-bc03-934b031377ca>
2009-03-26 17:50:01 +0000

    change fields of type varchar to TEXT

--- code/presley-server/src/main/resources/sql/ScriptPresley2.sql
+++ code/presley-server/src/main/resources/sql/ScriptPresley2.sql
@@ -6,28 +6,28 @@ use presley;
 CREATE TABLE projeto (
   nome VARCHAR(40) NOT NULL,
   ativo BOOL NULL,
-  endereco_Servidor_Leitura VARCHAR(200) NOT NULL,
-  endereco_Servidor_Gravacao VARCHAR(200) NOT NULL,
+  endereco_Servidor_Leitura TEXT NOT NULL,
+  endereco_Servidor_Gravacao TEXT NOT NULL,
   PRIMARY KEY(nome)
 );
 
 CREATE TABLE conhecimento (
-  nome VARCHAR(40) NOT NULL,
+  nome VARCHAR(70) NOT NULL,
   descricao VARCHAR(150) NULL,
   PRIMARY KEY(nome)
 );
 
 CREATE TABLE arquivo(
   id INTEGER UNSIGNED NOT NULL auto_increment,
-  arquivo_nome varchar(40) NOT NULL,
-  endereco_servidor varchar(200) NOT NULL,
+  arquivo_nome varchar(70) NOT NULL,
+  endereco_servidor TEXT NOT NULL,
   quantidadePalavras INTEGER NULL,
   PRIMARY KEY  (id)
 );
 
 CREATE TABLE conhecimento_has_arquivo (
   arquivo_id INTEGER UNSIGNED NOT NULL,
-  conhecimento_nome VARCHAR(40) NOT NULL,
+  conhecimento_nome VARCHAR(70) NOT NULL,
   PRIMARY KEY(arquivo_id, conhecimento_nome),
   INDEX conhecimento_has_arquivo_FKIndex1(arquivo_id),
   FOREIGN KEY (conhecimento_nome) REFERENCES conhecimento (nome) ON DELETE CASCADE,
@@ -37,7 +37,7 @@ CREATE TABLE conhecimento_has_arquivo (
 
 CREATE TABLE arquivo_has_palavras (
   arquivo_id INTEGER UNSIGNED NOT NULL,
-  palavra VARCHAR(40) NOT NULL,
+  palavra VARCHAR(50) NOT NULL,
   quantidade INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY(arquivo_id, palavra),
   INDEX arquivo_has_palavras(arquivo_id),
@@ -45,8 +45,8 @@ CREATE TABLE arquivo_has_palavras (
 );
 
 CREATE TABLE desenvolvedor (
-  email VARCHAR(40) NOT NULL,
-  nome VARCHAR(40) NULL,
+  email VARCHAR(50) NOT NULL,
+  nome VARCHAR(100) NULL,
   localidade VARCHAR(50) NULL,
   senha VARCHAR(40) NOT NULL,
   PRIMARY KEY(email)
@@ -54,13 +54,13 @@ CREATE TABLE desenvolvedor (
 
 CREATE TABLE problema (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  desenvolvedor_email VARCHAR(40) NOT NULL,
+  desenvolvedor_email VARCHAR(50) NOT NULL,
   atividade_id INTEGER NULL,
   descricao VARCHAR(100) NULL,
   resolvido BOOL NULL,
   dataRelato DATE NULL,
-  mensagem VARCHAR(250) NULL,
-  conhecimento_nome VARCHAR(40),
+  mensagem TEXT NULL,
+  conhecimento_nome VARCHAR(70),
   PRIMARY KEY(id),
   INDEX problema_FKIndex1(atividade_id),
   FOREIGN KEY (conhecimento_nome) REFERENCES conhecimento (nome) ON DELETE CASCADE
@@ -79,12 +79,12 @@ CREATE TABLE problema_has_classe (
 
 CREATE TABLE solucao (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  desenvolvedor_email VARCHAR(40) NOT NULL,
+  desenvolvedor_email VARCHAR(50) NOT NULL,
   problema_id INTEGER UNSIGNED NOT NULL,
   resolveu BOOL NULL,
   dataProposta DATE NULL,
-  mensagem VARCHAR(250) NULL,
-  retornoSolucao VARCHAR(250) NULL,
+  mensagem TEXT NULL,
+  retornoSolucao TEXT NULL,
   id_solucaoResposta INTEGER UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY(id),
   INDEX solucao_FKIndex1(problema_id),
@@ -93,7 +93,7 @@ CREATE TABLE solucao (
 
 CREATE TABLE mensagem (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  desenvolvedor_destino_email VARCHAR (40) NOT NULL,
+  desenvolvedor_destino_email VARCHAR (50) NOT NULL,
   problema_id INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (problema_id) REFERENCES problema (id) ON DELETE CASCADE,
@@ -101,16 +101,16 @@ CREATE TABLE mensagem (
 );
 
 CREATE TABLE conhecimento_pai_filho (
-  conhecimento_pai_nome VARCHAR(40) NOT NULL,
-  conhecimento_filho_nome VARCHAR(40) NOT NULL,
+  conhecimento_pai_nome VARCHAR(70) NOT NULL,
+  conhecimento_filho_nome VARCHAR(70) NOT NULL,
   PRIMARY KEY(conhecimento_pai_nome, conhecimento_filho_nome),
   FOREIGN KEY (conhecimento_pai_nome) REFERENCES conhecimento (nome) ON DELETE CASCADE,
   FOREIGN KEY (conhecimento_filho_nome) REFERENCES conhecimento (nome) ON DELETE CASCADE
 );
 
 CREATE TABLE desenvolvedor_has_conhecimento (
-  desenvolvedor_email VARCHAR(40) NOT NULL,
-  conhecimento_nome VARCHAR(40) NOT NULL,
+  desenvolvedor_email VARCHAR(50) NOT NULL,
+  conhecimento_nome VARCHAR(70) NOT NULL,
   grau real NOT NULL,
   PRIMARY KEY(desenvolvedor_email, conhecimento_nome),
   FOREIGN KEY (desenvolvedor_email) REFERENCES desenvolvedor (email) ON DELETE CASCADE, 
